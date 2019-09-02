274d9aa5cb6cd4c9a0e487b012bec3327d9e14c8
Jeremy Kithome <jeremy.kithome@andela.com>
2019-09-02 16:49:57 +0000

    feat: build a progress bar for react native

---
+++ index.js
@@ -0,0 +1,87 @@
+import React, {useRef, useState, useEffect} from 'react';
+import { Text, View, StyleSheet, Animated } from 'react-native';
+import Constants from 'expo-constants';
+
+// setInterval custom hook by Dan Abramov
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
+
+const App = () => {
+  let animation = useRef(new Animated.Value(0));
+  const [progress, setProgress] = useState(0);
+  useInterval(() => {
+    // update progress until 100
+    if(progress < 100) {
+      setProgress(progress + 5);
+    }
+  }, 1000);
+
+  useEffect(() => {
+    Animated.timing(animation.current, {
+      toValue: progress,
+      duration: 100
+    }).start();
+  },[progress])
+
+  const width = animation.current.interpolate({
+    inputRange: [0, 100],
+    outputRange: ["0%", "100%"],
+    extrapolate: "clamp"
+  })
+
+  return (
+    <View style={styles.container}>
+      <Text>
+        Loading…..
+      </Text>
+      <View style={styles.progressBar}>
+        <Animated.View style={[StyleSheet.absoluteFill], { backgroundColor: "#8BED4F", width }}/>
+      </View>
+      <Text>
+        {`${progress}%`}
+      </Text>
+
+    </View>
+  );
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'Column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  progressBar: {
+    flexDirection: 'row',
+    height: 20,
+    width: '100%',
+    backgroundColor: 'white',
+    borderColor: '#000',
+    borderWidth: 2,
+    borderRadius: 5
+  }
+});
